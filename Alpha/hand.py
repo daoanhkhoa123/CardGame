c4c934b8b286ec02a93eb8029c45ed25223d4c27
daoanhkhoa123 <158530319+daoanhkhoa123@users.noreply.github.com>
2024-02-24 23:41:57 +0700

    Update hand.py

--- Alpha/hand.py
+++ Alpha/hand.py
@@ -1,4 +1,8 @@
 from tkinter import *
+if __name__ != "__main__":
+    from Alpha.cards import Card
+else:
+    from cards import Card
 
 
 class _HandHolder:
@@ -21,40 +25,59 @@ class _HandHolder:
     """
 
     def __init__(self, master_frame, max_cards, row=0, column=0, padx=0, pady=0, ipadx=0, ipady=0, borderwidth=0, vertical=False, name=str(), money=0) -> None:
-        self.__frame = LabelFrame(master_frame, text=name, bd=borderwidth)
+        self.__frame: LabelFrame = LabelFrame(
+            master_frame, text=name, bd=borderwidth)
 
         if not vertical:
             self.__frame.pack(pady=pady, ipady=ipady)
         else:
             self.__frame.pack(padx=padx, ipadx=ipadx)
 
-        self.__card_slots = [Label] * max_cards
-        self.__image_list = [Image] * max_cards
-        self.__cards = [None] * max_cards
+        # lists delcaration
+        self._label_list: list[Label] = [None] * max_cards
+        self._card_list: list[Card] = [None] * max_cards
 
+        # preparing slots
         for i in range(max_cards):
-            self.__card_slots[i] = Label(self.__frame, text=str())
+            self._label_list[i] = Label(  # one label one card
+                self.__frame, text=str())
+
             if not vertical:
-                self.__card_slots[i].grid(
+                self._label_list[i].grid(
                     row=row, column=i, pady=pady, padx=padx)
             else:
-                self.__card_slots[i].grid(column=column, row=i,
-                                          pady=pady, padx=padx)
+                self._label_list[i].grid(column=column, row=i,
+                                         pady=pady, padx=padx)
 
-        self.__count_card = 0  # for convience in tracking card
+        self._count_card = 0  # for convience in tracking card
 
-    def get_hand(self):
-        """Iterate through hand's card
+    def get_card(self):
+        for i in range(self._count_card):
+            yield self._card_list[i]
 
-        Yields:
-            Card: card
+    def update_image(func) -> None:
+        """Update image decorator
+
+        This will update the image of the whole list.
+        Use with any function that would change the images.
+
+        Args:
+            func (function)
         """
-        for c in self.__cards:
-            if c is not None:
-                yield c
+        def inner(*args, **kwargs):
+            func(*args, **kwargs)
 
-    def hit_card(self, card) -> None:
+            """ render image """
+            self: _HandHolder = args[0]
+            for i in range(self._count_card):
+                self._label_list[i].config(image=self._card_list[i].image)
+
+        return inner
+
+    @update_image
+    def hit_card(self, card: Card) -> None:
         """Add one card into hand
+        NOTE: Default is cards are foleded
 
         Args:
             card (Card): card
@@ -62,27 +85,33 @@ class _HandHolder:
         Raises:
             IndexError: Maximum capacity reached
         """
-        if self.__count_card + 1 > len(self.__card_slots):
+        if self._count_card + 1 > len(self._label_list):
             raise IndexError(
                 "Invalid card: index out of range; Hand maximum capacity has reached.")
 
-        current_slot = self.__count_card
-        self.__count_card += 1
+        current_slot = self._count_card
+        self._count_card += 1
+
         # storing card and its image
-        self.__cards[current_slot] = card
-        self.__image_list[current_slot] = card.image
+        self._card_list[current_slot] = card
 
-        # show cards
-        self.__card_slots[current_slot].config(
-            image=self.__image_list[current_slot])
+    @update_image
+    def unfold_cards(self) -> None:
+        for i in range(self._count_card):
+            self._card_list[i].folded = False
 
-    def clear_hand(self) -> None:
-        """Clear all cards from hand
-        """
-        self.__count_card = 0
-        # not clearing cards slot because not needed
-        self.__image_list = [Image] * len(self.__image_list)
-        self.__cards = [None] * len(self.__cards)
+    def clear_cards(self) -> None:
+        # I can not use @update_image since it would be
+        # impossible to change the count_card attribute
+        # changing #update_image would be too complex
+
+        # since python garbage collector is shit, i have to do it manual
+
+        for i in range(self._count_card):
+            # self._label_list[i].config(image=None)
+            del (self._card_list[i])
+
+        self._count_card = 0
 
 
 class Hand(_HandHolder):
@@ -91,6 +120,7 @@ class Hand(_HandHolder):
                          padx, pady, ipadx, ipady, borderwidth, vertical, name)
 
         self.__score = 0
+        self.__money = 0
 
     @property
     def score(self) -> int:
@@ -103,7 +133,8 @@ class Hand(_HandHolder):
         joker_count = 0
         ace_count = 0
 
-        for card in self.get_hand():
+        # calculating the base points for all cards
+        for card in self.get_card():
             if card.rank == 15:  # joker
                 joker_count += 1
             elif card.rank == 14:  # ace
@@ -123,14 +154,13 @@ class Hand(_HandHolder):
                 self.__score -= 10  # switch to 1
 
         remain_score = 21 - self.__score
-        print(remain_score, ace_count, joker_count)
         if remain_score < joker_count:  # over 21
             return -1
 
-        if remain_score < 11 * joker_count:
-            return 21
+        if remain_score < 11 * joker_count:  # lower  than 21 but still
+            return 21                       # can use joker to get 21
 
-        self.__score += 11 * joker_count
+        self.__score += 11 * joker_count    # too low to get 21
 
         if self.__score > 21:
             return -1
